14c8436dd9e9452f5ab873c3055e49dc31a00652
Seemakhande <seemakhande68@gmail.com>
2026-01-02 23:53:48 +0530

    Added loop programs (for, while, do-while, break)

---
+++ Loops/FLoop.java
@@ -0,0 +1,10 @@
+
+public class FLoop {
+    public static void main(String args[]) {
+
+        // int i = 1;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Hello World");
+        }
+    }
+}
